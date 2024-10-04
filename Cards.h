580b468a5ee02beea9832f6e15fd6cf5044d509a
mattmonj <99099087+mattmonj@users.noreply.github.com>
2024-10-04 15:04:02 -0400

    Add files via upload

---
+++ Cards.h
@@ -0,0 +1,60 @@
+#pragma once
+#include <iostream>
+#include <vector>
+
+using namespace std;
+class Hand;
+class Deck;
+enum CardType { Bomb, Reinforcement, Blockade, Airlift, Diplomacy };
+class Card{
+
+public:
+    Card(CardType type);
+    Card();
+    ~Card();
+    void play(Hand* hand, Deck* deck);
+    CardType getType() const;
+  
+protected: 
+    string CardTypeToString(CardType type) const {
+        switch (type) {
+        case CardType::Bomb: return "Bomb";
+        case CardType::Reinforcement: return "Reinforcement";
+        case CardType::Blockade: return "Blockade";
+        case CardType::Airlift: return "Airlift";
+        case CardType::Diplomacy: return "Diplomacy";
+        default: return "Unknown";
+        }
+    }
+private:
+    CardType type;
+};
+
+class Deck: public Card{
+
+public: 
+    Deck(int CardsInDeck = 30);
+    ~Deck();
+   Card* draw();
+   void AddCardToDeck(Card* card);
+   void ShowCardInDeck();
+
+private: 
+    vector<Card*> deck;
+    Card* tempCard;
+};
+
+class Hand: public Deck{
+
+public:
+	Hand();
+	~Hand();
+    void AddCardToHand(Card* card);
+    void RemoveCardFromHand(Card* card);
+    void ShowCardInHand();
+    vector<Card*> getHandCards();
+   
+private:
+    vector<Card*> handCards;
+    
+};
---
+++ CardsDriver.cpp
@@ -0,0 +1,34 @@
+#include "Cards.h"
+
+void testCards(){
+    //creates a deck of Warzone cards
+    Deck* deck = new Deck;
+    cout << "The cards in the deck are : " << endl;
+    deck->ShowCardInDeck();
+    //create a hand object that is filled by drawing cards from the deck
+    Hand* hand = new Hand;
+        //draw cards from the deck to create a hand
+        for (int i = 0; i < 6; i++) {
+            hand->AddCardToHand(deck->draw());
+        }
+   
+    cout << "The cards in the hand are : " << endl;
+    hand->ShowCardInHand();
+
+    //iterating through cards in hand to play
+    for (const auto& card : hand->getHandCards()) {
+        card->play(hand, deck);
+    }
+
+    // Clean up
+    delete hand;
+    delete deck;
+}
+int main(){
+
+    testCards();
+
+}
+
+
+  
